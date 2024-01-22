962ee94c9344bc7b128ba427c2c4fa28414750a6
ikhela-04KK <kankoffi36@gmail.com>
2024-01-22 13:10:02 +0000

    add new file  and suceffuly run backend docker

--- apps/api/entry.sh
+++
@@ -1,9 +0,0 @@
-# set -e
-# until pg_isready -h "${PG_HOST}" -p 5432 -U "${PG_USER}"; do
-#     echo "$(date) - waiting for db to start"
-#     sleep 1
-# done
-# exec "$@"
-# npx prisma db push
-# npx prisma generate
-# npx prisma db seed
